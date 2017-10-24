5e24eacad1b24d6b0c2bf5fc4ecc6855f2e4732c
Arbaaz Meghani <amegha3@uic.edu>
2017-10-24 10:10:24 -0500

    added vive vr support. Need to test.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movementSpeed = 5.0f;
+	public GameObject cameraRig;
+
+	void FixedUpdate () {
+		float horizontalMovement = Input.GetAxis ("Horizontal");
+		float verticalMovement = Input.GetAxis ("Vertical");
+
+		horizontalMovement *= Time.deltaTime * movementSpeed;
+		verticalMovement *= Time.deltaTime * movementSpeed;
+
+		cameraRig.transform.Translate (horizontalMovement, 0.0f, verticalMovement);
+	}
+}
